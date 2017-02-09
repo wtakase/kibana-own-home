b849eb9546bb313f583d5f73f4efdc019d31205f
Wataru Takase <wataru.takase@kek.jp>
2017-02-09 12:04:18 +0900

    Fix issue #7

--- server/proxy/init_proxy.js
+++ server/proxy/init_proxy.js
@@ -10,6 +10,7 @@ import mapUri from './map_uri';
 import getKibanaIndexName from './get_kibana_index_name';
 import modifyPayload from './modify_payload';
 import initKibanaProxy from './init_kibana_proxy';
+import Boom from 'boom';
 
 module.exports = function(kbnServer) {
 
@@ -87,6 +88,15 @@ module.exports = function(kbnServer) {
             if (replacedIndex && payload[replacedIndex]) {
               payload[kbnServer.config().get('kibana.index')] = payload[replacedIndex];
               delete payload[replacedIndex];
+            }
+            if (replacedIndex && payload['error']) {
+              if (payload['status'] === 409) {
+                reply(Boom.conflict('plugin:own-home: document_already_exists_exception'));
+                return;
+              } else {
+                reply(Boom.badRequest('plugin:own-home: unhandled error'));
+                return;
+              }
             }
             reply(payload);
           });
