f9c26e29cf9008523d88e6f61233cdef3ba689ec
Wataru Takase <wataru.takase@kek.jp>
2017-09-14 12:57:19 +0900

    Support email address as a username (#52)

--- server/validate.js
+++ server/validate.js
@@ -7,9 +7,12 @@ export default function (server, request, remoteUser, kibanaIndexSuffix, callbac
 
   const config = server.config();
 
-  if (remoteUser == kibanaIndexSuffix) {
-    server.log(['plugin:own-home', 'debug'], 'kibanaIndexSuffix matches remote user name: ' + kibanaIndexSuffix);
-    setKibanaIndex(server, request, remoteUser, kibanaIndexSuffix);
+  // NOTE(wtakase): '@' replacement is necessary to support email address as a remoteUser name (issue #52).
+  //                To avoid any unexpected side-effects, the replacement is taken place only at this place.
+  if (remoteUser == kibanaIndexSuffix.replace('%40', '@')) {
+    const replacedKibanaIndexSuffix = kibanaIndexSuffix.replace('%40', '@')
+    server.log(['plugin:own-home', 'debug'], 'kibanaIndexSuffix matches remote user name: ' + replacedKibanaIndexSuffix);
+    setKibanaIndex(server, request, remoteUser, replacedKibanaIndexSuffix);
     return (callback === null) ? true : getGroups(server, request, remoteUser, callback);
   }
 
