1646eed397616341d400d2d03595cf779a5cd654
hameddhib <hameddhib@hotmail.com>
2019-03-21 17:23:31 +0100

    Update map_uri.js
    Import kibana dashboard in a shared tenant #115 #128

--- server/proxy/map_uri.js
+++ server/proxy/map_uri.js
@@ -4,6 +4,8 @@ import createKibanaIndex from './create_kibana_index';
 import migrateConfig from './migrate_config';
 import getReplacedIndex from './get_replaced_index';
 import createClient from './create_client';
+import getRemoteUser from '../get_remote_user';
+import validate from '../validate';
 
 export default function mapUri(server) {
   const config = server.config();
@@ -85,6 +87,8 @@ export default function mapUri(server) {
     var tenant = request.headers['tenant'];
 
     if(typeof tenant !== 'undefined' && tenant != null){
+		const remoteUser = getRemoteUser(server, request);
+        validate(server, request, remoteUser, tenant, null);
         replacedIndex = ".kibana_" + tenant;
     }
     
